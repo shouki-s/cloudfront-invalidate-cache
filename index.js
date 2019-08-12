a800d94b490b17118374acde7c508fabe8c5df80
Shouki SAKAMOTO <32442796+shouki-s@users.noreply.github.com>
2019-08-13 02:33:04 +0900

    move listDistributions and createInvalidation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-const AWS = require('aws-sdk');
 const yargs = require('yargs');
+const { listDistributions, createInvalidation } = require('./lib');
 
 const argv = yargs
   .option('cname', {
@@ -17,8 +17,6 @@ const argv = yargs
   .demandOption('cname')
   .help().argv;
 
-const cloudfront = new AWS.CloudFront();
-
 async function main() {
   let distributions = await listDistributions({});
   distributions = distributions.filter(dist => dist.Aliases.Items.includes(argv.cname));
@@ -29,40 +27,6 @@ async function main() {
     await createInvalidation(dist.Id);
   }
   console.log('Done!');
-}
-
-async function listDistributions() {
-  let distributions = [];
-
-  let data;
-  let marker;
-  do {
-    data = await cloudfront
-      .listDistributions({
-        Marker: marker,
-      })
-      .promise();
-    distributions = distributions.concat(data.DistributionList.Items);
-    marker = data.DistributionList.NextMarker;
-  } while (data.DistributionList.IsTruncated);
-
-  return distributions;
-}
-
-async function createInvalidation(distId) {
-  console.log(`Creating invalidation for ${distId}.`);
-  await cloudfront
-    .createInvalidation({
-      DistributionId: distId,
-      InvalidationBatch: {
-        CallerReference: String(new Date().getTime()),
-        Paths: {
-          Quantity: 1,
-          Items: ['/*'],
-        },
-      },
-    })
-    .promise();
 }
 
 main().catch(err => {
